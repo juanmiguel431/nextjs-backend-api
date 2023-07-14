f8ca1791a83b2af6a855c3a38ad709660c8382e7
juanmiguel431 <juanmiguel431@gmail.com>
2023-07-13 22:13:02 -0400

    Avoiding the code gets nested.

--- src/pages/api/feedback.ts
+++ src/pages/api/feedback.ts
@@ -18,28 +18,29 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Response>
 ) {
-  if (req.method === 'POST') {
-    const body = req.body as Feedback;
-
-    const newFeedback: Feedback = {
-      id: new Date().toISOString(),
-      email: body.email,
-      feedback: body.feedback
-    };
-
-    const filePath = path.join(process.cwd(), 'data', 'feedback.json');
-
-    const fileData = await fs.readFile(filePath, 'utf-8');
-    const data = JSON.parse(fileData) as Feedback[];
-    data.push(newFeedback);
-
-    await fs.writeFile(filePath, JSON.stringify(data));
-
-    res.status(201).json({
-      message: 'Success',
-      data: newFeedback
-    });
-  } else {
+  if (req.method !== 'POST') {
     res.status(400);
+    return;
   }
+
+  const body = req.body as Feedback;
+
+  const newFeedback: Feedback = {
+    id: new Date().toISOString(),
+    email: body.email,
+    feedback: body.feedback
+  };
+
+  const filePath = path.join(process.cwd(), 'data', 'feedback.json');
+
+  const fileData = await fs.readFile(filePath, 'utf-8');
+  const data = JSON.parse(fileData) as Feedback[];
+  data.push(newFeedback);
+
+  await fs.writeFile(filePath, JSON.stringify(data));
+
+  res.status(201).json({
+    message: 'Success',
+    data: newFeedback
+  });
 }
